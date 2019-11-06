d32e3a31b6d123304b9e4a017c594e1f8a8d03a6
Alexey <nikson1708@yandex.ru>
2019-11-06 16:45:51 +0300

    added csrf decorator

--- messenger/user_info/views.py
+++ messenger/user_info/views.py
@@ -1,8 +1,10 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 
 
+@csrf_exempt
 @require_http_methods(["GET"])
 def profil(request, pk=None):
     if pk is not None:
@@ -10,6 +12,7 @@ def profil(request, pk=None):
     return JsonResponse({'test': 'profil'})
 
 
+@csrf_exempt
 @require_http_methods(["GET"])
 def contacts(request):
     return JsonResponse({'test': 'contacts'})
