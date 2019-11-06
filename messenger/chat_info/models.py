1cb10941b59a468020e7e5be72c3bbd11285f2aa
Alexey <nikson1708@yandex.ru>
2019-11-06 21:33:53 +0300

    fuck yeah

--- messenger/chat_info/models.py
+++ messenger/chat_info/models.py
@@ -3,9 +3,9 @@ from user_info.models import User
 
 
 class Chat(models.Model):
-    is_group_chat = models.BooleanField(default=False)
-    topic = models.CharField(max_length=60)
-    last_message = models.CharField(max_length=5000)
+    is_group_chat = models.BooleanField('групповой или нет', default=False)
+    topic = models.CharField('название', max_length=60)
+    last_message = models.CharField('текст последнего сообщения',max_length=5000)
 
     class Meta:
         verbose_name = 'чат'
@@ -15,8 +15,8 @@ class Chat(models.Model):
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     chat = models.ForeignKey(Chat, on_delete=models.SET_NULL, null=True)
-    content = models.CharField(max_length=5000)
-    added_at = models.DateTimeField(default='1970-01-01 12:00:00')
+    content = models.CharField('текст сообщения', max_length=5000)
+    added_at = models.DateTimeField('дата добавления', default='1970-01-01 12:00:00')
 
     class Meta:
         verbose_name = 'сообщение'
@@ -37,8 +37,8 @@ class Attachment(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     chat = models.ForeignKey(Chat, on_delete=models.SET_NULL, null=True)
     message = models.ForeignKey(Message, on_delete=models.SET_NULL, null=True)
-    type_of_attachment= models.CharField(max_length=20)
-    url = models.CharField(max_length=100)
+    type_of_attachment = models.CharField('формат', max_length=20)
+    url = models.CharField('url', max_length=100)
 
     class Meta:
         verbose_name = 'прикрепление'
--- messenger/user_info/models.py
+++ messenger/user_info/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User (AbstractUser):
-    name = models.CharField(max_length=50)
-    nick = models.CharField(max_length=50)
-    avatar = models.CharField(max_length=100)
+    name = models.CharField('имя', max_length=50)
+    nick = models.CharField('ник', max_length=50)
+    avatar = models.CharField('ссылка на аватарку', max_length=100)
+    reg_date = models.DateTimeField('дата регистрации', default='1970-01-01 12:00:00')
 
     class Meta:
         verbose_name = 'пользователь'
